bb567ac5a7ac5091f3b6db1d3d976fc93ef21bab
Martez McDaniel <martez@Martezs-MacBook-Air.local>
2021-02-26 15:47:08 -0600

    created urls and templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Recipe
+
+def recipe_list(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
+
+def recipe_detail(request, pk):
+    recipe = Recipe.objects.get(id=pk)
+    return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
