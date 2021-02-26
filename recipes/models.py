fb9d20014701b04dc41124a61e8f1d5eaab5eff1
Martez McDaniel <martez@Martezs-MacBook-Air.local>
2021-02-25 18:15:42 -0600

    models created and migrated

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+    name=models.TextField()
+    description=models.TextField()
+    cuisine=models.TextField()
+    ingredients=models.TextField()
+    directions=models.TextField()
+    image=models.TextField()
+
+    def __str__(self):
+        return self.name
